16f637702c03ff278215a170ce949f00c2a9fdeb
Matthias Pfefferle <pfefferle@gmail.com>
2022-05-16 08:47:24 +0200

    use WordPress style instead of PSR-2

--- src/ErrorLogLogger.php
+++ src/ErrorLogLogger.php
@@ -6,28 +6,27 @@ use Psr\Log\AbstractLogger;
 
 class ErrorLogLogger extends AbstractLogger
 {
-    /**
-     * Logs with an arbitrary level.
-     *
-     * @param mixed  $level
-     * @param string $message
-     * @param array $context
-     *
-     * @return void
-     */
-    public function log($level, string $message, array $context = [])
-    {
-        if (defined('WP_DEBUG') && WP_DEBUG) {
-            if (defined('WP_DEBUG_LOG') && WP_DEBUG_LOG) {
-                error_log(
-                    sprintf(
-                        '%s: %s. Details: %s',
-                        $level,
-                        trim( $message, '.' ),
-                        json_encode( $context )
-                    )
-                );
-            }
-        }
-    }
+	/**
+	 * Logs with an arbitrary level.
+	 *
+	 * @param mixed  $level
+	 * @param string $message
+	 * @param array $context
+	 *
+	 * @return void
+	 */
+	public function log( $level, $message, $context = array() ) {
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			if ( defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG ) {
+				error_log(
+					sprintf(
+						'%s: %s. Details: %s',
+						$level,
+						trim( $message, '.' ),
+						json_encode( $context )
+					)
+				);
+			}
+		}
+	}
 }
